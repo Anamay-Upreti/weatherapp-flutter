9e7cbbd4cf9eb029339c949cc82bdaafefc3ee8c
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-13 22:35:44 +0530

    added-initial-file

---
+++ lib/widgets/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  final primaryColor = Color.fromARGB(255, 134, 107, 252);
+  final secondaryColor = const Color(0xffa1c6fd);
+  final tertiaryColor = const Color(0xff205cf1);
+  final blackColor = const Color(0xff000000);
+
+  final greyColor = const Color(0xffd9adb9);
+}
