3c66744c4d47c623612305506ca7ca55e6b55255
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-14 22:07:58 +0530

    added-linear gradint

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -7,4 +7,18 @@ class Constants{
   final blackColor = const Color(0xff000000);
 
   final greyColor = const Color(0xffd9adb9);
+
+  final linearGradientBlue = const LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.topLeft,
+    colors: <Color>[Color(0xffABcff2), Color.fromARGB(255, 95, 155, 215)],
+     stops: [0.0, 1.0]
+  );
+
+  final linearGradientPurple = const LinearGradient(
+    begin: Alignment.bottomRight,
+    end: Alignment.topLeft,
+    colors: [Color(0xff51087e), Color(0xff6C0BA9)],
+    stops: [0.0, 1.0]
+  )
 }
